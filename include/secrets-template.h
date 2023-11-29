491156192024b7235a1377ef7338d1a3a27d57e0
nicolas <nicolas.mangini@ing.austral.edu.ar>
2023-11-28 21:00:51 -0300

    fix: secrets template

--- include/secrets-template.h
+++ include/secrets-template.h
@@ -1,13 +1,15 @@
-
-#define SECRET
-#define THINGNAME "Pepe" // Cambiar
 #define MACHINE_ID ""
 
 const char WIFI_SSID[] = "";                               // Su SSID
 const char WIFI_PASSWORD[] = "";                              // Su contrasenia
 const char IOT_ENDPOINT[] = ""; // FQDN del broker
 
-// CA certificate - Copiar todo el texto entre -----BEGIN CERTIFICATE----- y -----END CERTIFICATE----
+// MQTT topics
+const char TOPIC1[] = "";
+const char TOPIC2[] = "";
+const char TOPIC3[] = "";
+
+// CA certificate
 static const char CERT_CA[] = R"EOF(
 -----BEGIN CERTIFICATE-----
 -----END CERTIFICATE-----
